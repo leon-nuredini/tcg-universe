9f2a1c34ade9cc5fbf733e135e8cd55375123158
Leon <33429205+leon-nuredini@users.noreply.github.com>
2025-03-07 14:25:16 +0100

    Added a header and a footer.

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo from '../assets/logo.png'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+            <Container>
+                <Navbar.Brand href='/'>
+                    <img src={logo} alt='TCG Universe Logo' />
+                    TCG Universe
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-menu' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='ms-auto'>
+                        <Nav.Link href='/cart'><FaShoppingCart />Cart</Nav.Link>
+                        <Nav.Link href='/login'><FaUser />Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
