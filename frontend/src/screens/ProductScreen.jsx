1d5fc0b21b1a5dce9dd62818f69dbf2041b0aba2
Leon <33429205+leon-nuredini@users.noreply.github.com>
2025-05-14 16:25:57 +0200

    Added a loader and message prompt in the frontend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 export const ProductScreen = () => {
@@ -12,9 +14,9 @@ export const ProductScreen = () => {
   return (
     <>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>error?.data?.message || error.error</div>
+        <Message variant='danger'>error?.data?.message || error.error</Message>
       ) : (
         <>
           <Link className="btn btn-light my-3" to="/">
